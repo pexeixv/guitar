34868cc87619711a364eb0e6232a3a436de4a478
pexeixv <gavineditz@gmail.com>
2022-11-18 10:33:23 +0530

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'http://jeffkershner.com/',
+  integrations: [
+    tailwind(),
+    sitemap()
+  ]
 });
